28f80210f7eb513353875b2f4081d3eb9547d914
jackietrang <42923696+jackietrang@users.noreply.github.com>
2021-03-13 18:56:23 -0800

    :sparkles: feat: kanban updates

---
+++ OneDrive/Desktop/kanban assignment/dbtest.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import unittest
+from app import db
+
+class appDBTests(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Creates a new database for the unit test to use
+        """
+        self.app = Flask(__name__)
+        db.init_app(self.app)
+        with self.app.app_context():
+            db.create_all()
+            self.populate_db() 
+
+    def tearDown(self):
+        """
+        Ensures that the database is emptied for next unit test
+        """
+        self.app = Flask(__name__)
+        db.init_app(self.app)
+        with self.app.app_context():
+            db.drop_all()
+
+if __name__ == "__main__":
+    unittest.main()
